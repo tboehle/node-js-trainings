1ddae94d7080b774f3a3661359310c619967ffe1
Thorben BOEHLE <thorben.boehle@piterion.com>
2020-03-10 09:50:25 +0100

    Advanced events emitter

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 // Modules and require
 var events = require('events');
+var util = require('util');
 
-var myEmmiter = new events.EventEmitter()
+var Person = function (name) {
+    this.name = name;
+};
+// everything crated using the Person constructor is able to attach custom events
+util.inherits(Person, events.EventEmitter)
 
-myEmmiter.on('someEvent', function (msg) {
-    console.log(msg);
-})
+var james = new Person('james');
+var mary = new Person('mary');
+var ryu = new Person('ryu');
+var people = [james, mary, ryu];
 
-myEmmiter.emit('someEvent', 'the event was emitted')
+// define event speak for each person
+people.forEach(function (person) {
+    // we can attach custom events to the people now
+    person.on('speak', function (msg) {
+        console.log(person.name + ' said ' + msg);
+    });
+});
+
+// call defined events
+james.emit('speak', 'hey dudes');
+ryu.emit('speak', 'I am hungry!');
