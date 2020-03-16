d18f6712107bea0c15ad3c1ca6126af9fcdbf427
Thorben BOEHLE <thorben.boehle@piterion.com>
2020-03-16 17:28:20 +0100

    Creating and Deleting folders

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var fs = require('fs');
 
-// Sync is blocking the code underneath
-fs.readFile('readMe.txt', 'utf8', function(err, data) {
-    // asynchronous function -> need for callback function to fire when the process is complete
-    fs.writeFile('writeMe.txt', data, (err)=>{
+fs.mkdir('stuff', function(){
+    fs.readFile('readMe.txt', 'utf8', function(err, data){
         if (err) {
             throw err;
         }
-        console.log('Successfully saved file');
+        fs.writeFile('./stuff/writeMe.txt', data, function(err){
+            if (err) {
+                throw err;
+            }
+        });
     });
 });
+
+// Can't activate function because of asynchronous creation of folder and file
+/*
+fs.unlink('./stuff/writeMe.txt', function(){
+    fs.rmdir('stuff', function(err){
+        if (err) {
+            throw err;
+        }
+    });
+});*/
