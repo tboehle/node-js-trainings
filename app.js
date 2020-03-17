5937ac21905d64a38f44ac41a42c5e8770a1806b
Thorben BOEHLE <thorben.boehle@piterion.com>
2020-03-17 08:31:27 +0100

    Writeable streams

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var fs = require('fs');
 
 // will be parsed through buffer
 var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt')
 
 myReadStream.on('data', function (chunk) {
     console.log('new chunk received:');
-    console.log(chunk);
+    myWriteStream.write(chunk);
 });
 
 
