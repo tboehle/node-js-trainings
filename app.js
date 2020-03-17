b470d69f99d9ea587ac208822c88d2b742123847
Thorben BOEHLE <thorben.boehle@piterion.com>
2020-03-17 08:56:49 +0100

    Writable stream with server

--- app.js
+++ app.js
@@ -5,23 +5,19 @@ var fs = require('fs');
 var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
 var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt')
 
-myReadStream.on('data', function (chunk) {
-    console.log('new chunk received:');
-    myWriteStream.write(chunk);
-});
+// listening to readStream and piping it to writeStream
+myReadStream.pipe(myWriteStream);
 
 
-/*
 var server = http.createServer(function(req, res){
     console.log('request was made: ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hey guys');
+    // data is parsed through buffer sequences
+    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+
+    // listening to readStream (data) and piping it to response writable stream
+    myReadStream.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1');
 console.log('yo dawgs, now listening to port 3000');
-
-module.exports = {
-    server: server
-};
-*/
