f2d0de56c3302bade86727e138df4d204e8b9aa4
Thorben BOEHLE <thorben.boehle@piterion.com>
2020-03-26 22:44:52 +0100

    Express route params

--- app.js
+++ app.js
@@ -13,9 +13,8 @@ app.get('/contact', function(req, res) {
     fs.createReadStream(__dirname + '/contact.html').pipe(res);
 });
 
-app.get('/contact', function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/contact.html').pipe(res);
+app.get('/profile/:id', function(req, res) {
+    res.send('You requested a profile with the id of ' + req.params.id);
 });
 
 app.listen(3000);
