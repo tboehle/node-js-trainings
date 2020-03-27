4bdf7d32efcb40961990b2c0b0274d2914b6279c
Thorben BOEHLE <thorben.boehle@piterion.com>
2020-03-27 12:12:33 +0100

    Partial templates with ejs

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render('index', {person: req.params.name});
+    res.render('index');
 });
 
 app.get('/contact', function(req, res) {
-    res.render('contact', {person: req.params.name});
+    res.render('contact');
 });
 
 app.get('/profile/:name', function(req, res) {
