4303815c1e5479c70238cf19294bbeada3c6aefd
Thorben BOEHLE <thorben.boehle@piterion.com>
2020-03-28 12:10:49 +0100

    Add support for mongoose

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,15 @@
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose'); 
+
+// Connect to the database
+mongoose.connect('mongodb://test:test@localhost:5000/todo')
+
+// Create a schema
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+// Todo model
+var Todo = mongoose.Model('Todo', todoSchema);
 
 
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
@@ -8,19 +19,26 @@ const urlencodedParser = bodyParser.urlencoded({extended: false});
 module.exports = function(app) {
 
     app.get('/todo', function(req, res) {
-
-        res.render('todo', {todos: data});
+        // get data from mongodb and pass it to view
+        Todo.find({}, function(err, data) {
+            if (err) throw err;
+            res.render('todo', {todos: data});
+        });
     });
 
     app.post('/todo', urlencodedParser, function(req, res) {
-        data.push(req.body);
-        res.json(data);
+        // get data from the view and add it to mongodb
+        var newTodo = Todo(req.body).save(function(err, data) {
+            if (err) throw err;
+            res.json(data);
+        })
     });
 
     app.delete('/todo/:item', function(req, res) {
-        data = data.filter(function(todo) {
-            return todo.item.replace(/ /g, '-') !== req.params.item;
-        })
-        res.json(data);
+        // delete the requested item from mongodb
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data) {
+            if (err) throw err;
+            res.json(data);
+        });
     });
 };
